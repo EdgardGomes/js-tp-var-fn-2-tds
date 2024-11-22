4031add2c4eed3a0de8a7a19b677dda6de1f3227
EdgardGomes <160244330+EdgardGomes@users.noreply.github.com>
2024-11-22 08:17:44 -0300

    carga_js-tp-var-fn

---
+++ script.js
@@ -0,0 +1,57 @@
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero - segundoNumero;
+console.log(operacaoMatematica);
+
+//ponto flutuante
+
+const numeroPontoFluente = 3.3;
+const pontoFluenteSemZero = .5;
+
+const novaOperacao = primeiroNumero/ numeroPontoFLuente;
+
+console.log (novaOperacao)
+
+//Nan
+
+const alura = "Alura";
+console.log(alura*primeiroNumero)
+
+//var
+
+var altura = 5;
+var comprimento = 7;
+var area
+
+area = altura * comprimento
+console.log(area)
+
+let form = 'retângulo';
+let altura = 5;
+let comprimento = 7;
+let area;
+
+if (forma === 'retângulo'){
+area = altura * comprimento;
+}
+else {
+area = (altura * comprimento)/2;
+}
+
+console.log (area)
+
+const forma = 'triângulo';
+const altura = 5;
+const comprimento = 7;
+let area;
+if (forma === 'quadrado') {
+area = altura *comprimento;
+}
+else {
+area = (altura*comprimento)/2;
+}
+
+console.log(area)
